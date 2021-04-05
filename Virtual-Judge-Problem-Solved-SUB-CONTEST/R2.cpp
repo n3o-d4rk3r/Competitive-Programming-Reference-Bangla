3fc6f4d3f0f3b7be7193a4bdeec1324ef5e66bb6
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-04-06 02:20:04 +0600

    Create R2.cpp

---
+++ Virtual-Judge-Problem-Solved-SUB-CONTEST/R2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// Aryan Kabir
+// https://facebook.com/ictd.kabir
+
+int main()
+
+{
+    int r1,r2,S;
+    cin>>r1>>S;
+
+    r2=((2*S)-r1);
+    cout<<r2<<endl;
+    return 0;
+
+
+}
