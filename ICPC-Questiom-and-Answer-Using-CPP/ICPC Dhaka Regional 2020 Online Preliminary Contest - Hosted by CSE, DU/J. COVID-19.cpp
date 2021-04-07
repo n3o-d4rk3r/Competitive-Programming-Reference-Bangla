2903a3fe68e17dcd056bcce0eef012becf3fa357
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-04-08 02:50:02 +0600

    Add files via upload

---
+++ ICPC-Questiom-and-Answer-Using-CPP/ICPC Dhaka Regional 2020 Online Preliminary Contest - Hosted by CSE, DU/J. COVID-19.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    ///                Moon
+    ///                Kabir
+    ///                Mazharul
+
+    long long n, x, people = 1;
+
+
+    cin>>n;
+
+    x = n / 6;
+
+    cout<<people + x<<endl;
+
+    return 0;
+}
+
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+//1 
