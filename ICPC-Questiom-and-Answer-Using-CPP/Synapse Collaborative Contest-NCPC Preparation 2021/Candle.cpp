4d56deeac4643ecbe062f356dac761d1173c8a39
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-06-25 18:06:45 +0600

    Solved by Aryan Kabir

---
+++ ICPC-Questiom-and-Answer-Using-CPP/Synapse Collaborative Contest-NCPC Preparation 2021/Candle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Team: SUB-SUBSequence
+Member:
+    Aryan Kabir
+    Anujit Deb
+    Majharul Islam
+*/
+
+int main()
+{
+    int d, r, t, cndls = 0, crt = 0;
+    cin>>d>>r>>t;
+    for(int age=1;age<=2000;age++)
+    {
+        if(age >= 4)    cndls+=age,   crt += age;
+        if(age-d >= 3)  cndls+=age-d;
+        if(r+t == cndls) {
+
+                cout<<r-crt;
+
+            return 0;
+        }
+    }
+}
